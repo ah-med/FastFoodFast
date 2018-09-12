3d1a1d3884aba849b756aee242555a4255d15619
ah-med <ahmedomosanya@gmail.com>
2018-09-12 16:19:12 +0100

    feat(place order) - user place new order
    - create  POST /orders route
    - create OrderControllers.create
    - add listening port 9000
    - use body-parser to parse request body
    - use trimmer to trim request fields
    - add validation schema
    - add validator function
    - add validateOrder function
    
    [Finishes #160438258]

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import trimmer from 'trim-request-body';
 
+import index from './routes/index';
+
 const fastfoodApp = express();
 const port = process.env.PORT || 9000;
 
@@ -16,7 +18,9 @@ fastfoodApp.use(trimmer);
 
 fastfoodApp.listen(port);
 
-fastfoodApp.use('/', (req, res) => {
+fastfoodApp.use('/', index);
+
+fastfoodApp.use('*', (req, res) => {
   res.status(200).json({
     status: 200,
     message: 'Welcome to edited FastFoodFast Application',
---
+++ server/validations/validationSchema.js
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+import JoiPhoneNumber from 'joi-phone-number';
+
+const validatePhone = Joi.extend(JoiPhoneNumber);
+
+const foodItemObj = Joi.object().keys({
+  itemId: Joi.number().positive().required(),
+  quantity: Joi.number().positive().min(1).max(100)
+    .required(),
+}).required();
+
+const Schema = {
+  placeOrderSchema: {
+    name: Joi.string().min(4).max(20).required(),
+    phoneNo: validatePhone.string().phoneNumber().min(11).max(13)
+      .required(),
+    address: Joi.string().min(7).max(50).required(),
+    foodItems: Joi.array().items(foodItemObj).required(),
+  },
+};
+
+export default Schema;
