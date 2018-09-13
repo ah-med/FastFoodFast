baeb9015b63cdd3ef933f99c966e4bd1c69b0adb
ah-med <ahmedomosanya@gmail.com>
2018-09-13 12:00:32 +0100

    feat(order) - fetch a specific order
    - add test for OrderController.getOrder

--- server/test/controllers/OrderControllers.js
+++ server/test/controllers/OrderControllers.js
@@ -71,3 +71,26 @@ describe('OrderController.update', () => {
       });
   });
 });
+
+describe('OrderController.getOrder', () => {
+  // create an order first get the Id and use it to update another order
+  it('should accept an order', (done) => {
+    chai.request(app)
+      .post('/orders')
+      .send({
+        name: 'Joe Jackson', phoneNo: '12345678901', address: 'my home town', foodItems: [{ itemId: 1, quantity: 4 }],
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        const { orderId } = res.body.data[0];
+        chai.request(app)
+          .get(`/orders/${orderId}`)
+          .end((err, res) => {
+            expect(res.body).to.have.property('status');
+            expect(res.body).to.have.property('data');
+            expect(res.status).to.equal(200);
+            done();
+          });
+      });
+  });
+});
