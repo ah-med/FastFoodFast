2daa93eeb5f673a05636555b65c4b953b5c90a93
ah-med <ahmedomosanya@gmail.com>
2018-09-20 11:33:58 +0100

    feat(login)  - login a user
    - add validations for login endpoint
    - add /auth/login route
    
    [Finishes #160630047]

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,13 +2,19 @@ import express from 'express';
 import validateAuth from '../validations/validateAuth';
 import AuthController from '../controllers/AuthController';
 import checkExistingAccount from '../middlewares/checkExistingAccount';
+import fetchUserData from '../middlewares/fetchUserData';
+import verifyUserCredentials from '../middlewares/verifyUserCredentials';
+import assignLoginToken from '../middlewares/assignLoginToken';
 
 
-const { validateSignup } = validateAuth;
+const { validateSignup, validateLogin } = validateAuth;
 const router = express.Router();
 
 
 // Register a user
 router.post('/signup', validateSignup, checkExistingAccount, AuthController.signUp);
+
+// Login a user
+router.post('/login', validateLogin, fetchUserData, verifyUserCredentials, assignLoginToken, AuthController.login);
 
 export default router;
