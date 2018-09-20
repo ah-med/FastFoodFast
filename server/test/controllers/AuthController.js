78db3a2607230c4036e514a25c248c87fae31644
ah-med <ahmedomosanya@gmail.com>
2018-09-20 06:34:51 +0100

    feat(signup) - add test for endpoint
    - add test for signup endpoint
    
    [Finishes #160630063]

---
+++ server/test/controllers/AuthController.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../index';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('Authentication controller methods', () => {
+  it('should register a new user account', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        firstName: 'afirstname', lastName: 'alastname', email: 'newUser@mail.com', password: '12345678'
+      })
+      .end((err, res) => {
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.have.property('userId');
+        expect(res.body.data).to.have.property('firstName');
+        expect(res.body.data).to.have.property('email');
+        expect(res.body.data).to.have.property('lastName');
+        expect(res.body.data).to.have.property('role');
+        expect(res.status).to.equal(201);
+        done();
+      });
+  });
+});
