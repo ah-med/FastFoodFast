f6f12c80f48bebcd3ca2968203fa76094ee650dc
ah-med <ahmedomosanya@gmail.com>
2018-09-20 15:22:13 +0100

    feat(admin) - admin add meal to menu
    - add route for POST /menu endpoint
    
    [Finishes #160630255]

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -7,21 +7,7 @@ import MenuController from '../controllers/MenuController';
 
 const router = express.Router();
 
-
-/**
-*   verifyToken
-
-    verifyRole.admin
-
-    ValidateMeal
-
-    MenuController.addMeal
- * * */
-
 // Add a meal option to the menu
 router.post('/', verifyToken, verifyRole.admin, validateMeal, MenuController.addMeal);
-
-// Get available menu
-router.get('/', verifyToken, verifyRole.admin /*, MenuController.fetchMenu */);
 
 export default router;
