cef3c42acc4741a4f0a3fda05baa126140cff7ae
ah-med <ahmedomosanya@gmail.com>
2018-09-20 13:37:27 +0100

    feat(signup) - register a user
    - change pass to hashedPassword
    - change param to parameters
    
    [Finishes #160630063]

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -16,11 +16,11 @@ class AuthController {
     const {
       firstName, lastName, email, password
     } = req.body;
-    const pass = bcrypt.hashSync(password, 10);
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
     const text = 'INSERT INTO users(first_name, last_name, email, password, role) values($1, $2, $3, $4, $5) RETURNING user_id';
-    const param = [firstName, lastName, email, pass, 'user'];
-    db.query(text, param, (err, user) => {
+    const parameters = [firstName, lastName, email, hashedPassword, 'user'];
+    db.query(text, parameters, (err, user) => {
       if (err) return errors.serverError(res);
       return res.status(201).json({
         message: 'Account created successfully',
