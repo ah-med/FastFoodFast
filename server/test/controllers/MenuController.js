e87cf471f79dbcf3200d2dac6e91b78eef8f3e2a
ah-med <ahmedomosanya@gmail.com>
2018-09-21 10:43:55 +0100

    feat(menu) - get available menu
    - add test for POST /menu
    
    [Finishes #160630196]

--- server/test/controllers/MenuController.js
+++ server/test/controllers/MenuController.js
@@ -45,4 +45,36 @@ describe('Menu controller methods', () => {
           });
       });
   });
+  it('should get all available menu', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'admin@mail.com', password: process.env.ADMIN_PASS
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        adminToken = bearer + res.body.data.token;
+        chai.request(app)
+          .post('/api/v1/menu')
+          .set('Authorization', adminToken)
+          .send({
+            itemName: 'Chicken Burger',
+            quantity: 8,
+            price: 1200,
+            imageUrl: 'https://res.cloudinary.com/ah-med/image/upload/v1537442340/fastfoodImage/continental/burger-chicken.jpg',
+            category: 'Continental'
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(201);
+            chai.request(app)
+              .get('/api/v1/menu')
+              .end((err, res) => {
+                expect(res.status).to.equal(200);
+                expect(res.body).to.have.property('status');
+                expect(res.body).to.have.property('data');
+                done();
+              });
+          });
+      });
+  });
 });
