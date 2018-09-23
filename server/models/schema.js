dce1b3d025b42d95ad4992204e47311dd687370f
ah-med <ahmedomosanya@gmail.com>
2018-09-23 17:18:37 +0100

    chore(database) - modify database
    - add address column to orders table
    - remove delivery_address table
    - add phone_number to orders table
    - change process.env.ADMIN_PASS to process.env.ADMIN_PASSWORD
    
    [Finishes #160709507]

--- server/models/schema.js
+++ server/models/schema.js
@@ -5,7 +5,7 @@ import db from './db';
 
 dotenv.config();
 
-const adminPassword = bcrypt.hashSync(process.env.ADMIN_PASS, 10);
+const adminPassword = bcrypt.hashSync(process.env.ADMIN_PASSWORD, 10);
 
 const tableSchema = `
     DROP TABLE IF EXISTS users CASCADE;
@@ -14,84 +14,46 @@ const tableSchema = `
 
     DROP TABLE IF EXISTS menu CASCADE;
 
-    DROP TABLE IF EXISTS order_items;
-
-    DROP TABLE IF EXISTS delivery_address;
-
     CREATE TABLE "users" (
-        "user_id" serial NOT NULL,
-        "first_name" TEXT,
-        "last_name" TEXT,
-        "email" TEXT NOT NULL UNIQUE,
-        "password" TEXT NOT NULL,
-        "role" TEXT NOT NULL,
-        CONSTRAINT users_pk PRIMARY KEY ("user_id")
+      "user_id" serial NOT NULL,
+      "first_name" TEXT,
+      "last_name" TEXT,
+      "email" TEXT NOT NULL UNIQUE,
+      "password" TEXT NOT NULL,
+      "role" TEXT NOT NULL,
+      CONSTRAINT users_pk PRIMARY KEY ("user_id")
     ) WITH (
-    OIDS=FALSE
+      OIDS=FALSE
     );
-
-
-
+    
     CREATE TABLE "orders" (
-        "order_id" serial NOT NULL,
-        "user_id" int NOT NULL,
-        "status" TEXT NOT NULL,
-        "date_created" TIMESTAMP NOT NULL,
-        "last_updated" TIMESTAMP NOT NULL,
-        "total_amount" int NOT NULL,
-        CONSTRAINT orders_pk PRIMARY KEY ("order_id")
+      "order_id" serial NOT NULL,
+      "status" TEXT NOT NULL,
+      "user_id" int NOT NULL,
+      "address" TEXT NOT NULL,
+      "phone_number" TEXT NOT NULL,
+      "total_amount" int NOT NULL,
+      "date_created" TIMESTAMP NOT NULL,
+      "last_updated" TIMESTAMP NOT NULL,
+      "order_items" json NOT NULL,
+      CONSTRAINT orders_pk PRIMARY KEY ("order_id")
     ) WITH (
-    OIDS=FALSE
+      OIDS=FALSE
     );
-
-
-
+    
     CREATE TABLE "menu" (
-        "food_id" serial NOT NULL,
-        "no_of_times_ordered" int NOT NULL DEFAULT '0',
-        "item_name" TEXT NOT NULL,
-        "quantity" TEXT NOT NULL,
-        "price" int NOT NULL,
-        "image_url" TEXT NOT NULL,
-        "category" TEXT NOT NULL,
-        CONSTRAINT menu_pk PRIMARY KEY ("food_id")
+      "food_id" serial NOT NULL,
+      "food_name" TEXT NOT NULL,
+      "price" int NOT NULL,
+      "image_url" TEXT NOT NULL,
+      "category" TEXT NOT NULL,
+      CONSTRAINT menu_pk PRIMARY KEY ("food_id")
     ) WITH (
-    OIDS=FALSE
+      OIDS=FALSE
     );
-
-
-
-    CREATE TABLE "order_items" (
-        "order_id" int NOT NULL,
-        "food_id" int NOT NULL,
-        "quantity_ordered" int NOT NULL
-    ) WITH (
-    OIDS=FALSE
-    );
-
-
-
-    CREATE TABLE "delivery_address" (
-        "order_id" int NOT NULL,
-        "street_address" TEXT NOT NULL,
-        "city" TEXT NOT NULL,
-        "state" TEXT NOT NULL,
-        "phone_no" bigint NOT NULL
-    ) WITH (
-    OIDS=FALSE
-    );
-
-
-
-
+    
     ALTER TABLE "orders" ADD CONSTRAINT "orders_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("user_id");
-
-
-    ALTER TABLE "order_items" ADD CONSTRAINT "order_items_fk0" FOREIGN KEY ("order_id") REFERENCES "orders"("order_id");
-    ALTER TABLE "order_items" ADD CONSTRAINT "order_items_fk1" FOREIGN KEY ("food_id") REFERENCES "menu"("food_id");
-
-    ALTER TABLE "delivery_address" ADD CONSTRAINT "delivery_address_fk0" FOREIGN KEY ("order_id") REFERENCES "orders"("order_id");
-
+    
     INSERT INTO users ( 
         first_name, last_name, 
         email, 
