241e9016ff3f6170f77069fb2fceeb37bb17d2e6
ah-med <ahmedomosanya@gmail.com>
2018-09-24 11:17:16 +0100

    feat(addMeal) - admin add a meal to the menu
    - change variable name itemName to foodName
    
    [Finishes #160711749]

--- server/middlewares/checkExistingMeal.js
+++ server/middlewares/checkExistingMeal.js
@@ -2,13 +2,13 @@ import db from '../models/db';
 import errors from '../controllers/errors';
 
 const checkExistingMeal = (req, res, next) => {
-  const itemName = req.body.itemName.toLowerCase();
+  const foodName = req.body.foodName.toLowerCase();
 
   const text = '(select food_name from menu)';
 
-  db.query(text, (err, foodName) => {
+  db.query(text, (err, food) => {
     if (err) errors.serverError(res);
-    const findName = foodName.rows.find(val => val.food_name.toLowerCase() === itemName);
+    const findName = food.rows.find(val => val.food_name.toLowerCase() === foodName);
     if (findName) {
       return res.status(409).json({
         error: {
