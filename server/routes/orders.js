9ede2ae71fd23da2226e77e97ad3dc02f6e22b17
ah-med <ahmedomosanya@gmail.com>
2018-09-24 16:46:48 +0100

    feat(order) - place an order for food
    - create route for POST /orders endpoint
    - add middleware to calculate total amount
    - add middleware to verify food items in database
    - add verifyRole.user middleware
    - modify fetchUserData middleware
    - remove routes/index.js
    
    [Delivers  #160630126]

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import validate from '../validations/validate';
+<<<<<<< HEAD
 import verify from '../middlewares/verify';
+=======
+>>>>>>> feat(order) - place an order for food
 import verifyFoodItem from '../middlewares/verifyFoodItem';
 import verifyToken from '../middlewares/verifyToken';
 import getTotalAmount from '../middlewares/getTotalAmount';
@@ -10,18 +13,25 @@ import OrderController from '../controllers/OrderController';
 
 const router = express.Router();
 
+<<<<<<< HEAD
 const { validateOrder, validateOrderId, validateOrderStatus } = validate;
 const { verifyOrderId } = verify;
+=======
+const { validateOrder } = validate;
+>>>>>>> feat(order) - place an order for food
 
 
 // place an order
 router.post('/', verifyToken, verifyRole.user, validateOrder, verifyFoodItem, assignOrderStatus.newOrder, getTotalAmount, OrderController.create);
 
+<<<<<<< HEAD
 // Get a specific order
 router.get('/:orderId', verifyToken, verifyRole.admin, validateOrderId, verifyOrderId, OrderController.getOrder);
 
 // update order
 router.put('/:orderId', verifyToken, verifyRole.admin, validateOrderStatus, validateOrderId, verifyOrderId, assignOrderStatus.update, OrderController.updateStatus);
 
+=======
+>>>>>>> feat(order) - place an order for food
 
 export default router;
