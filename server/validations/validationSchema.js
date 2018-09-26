c1cddc95e2f05145280b3a51aa19bd993197c7b2
ah-med <ahmedomosanya@gmail.com>
2018-09-26 21:56:33 +0100

    feat(validation) - add more validation
    - add validation for user input
    - update tests
    
    [Finishes #160784820]

--- server/validations/validationSchema.js
+++ server/validations/validationSchema.js
@@ -1,7 +1,4 @@
 import Joi from 'joi';
-import JoiPhoneNumber from 'joi-phone-number';
-
-const validatePhone = Joi.extend(JoiPhoneNumber);
 
 const foodItemObj = Joi.object().keys({
   foodId: Joi.number().positive().required(),
@@ -12,8 +9,7 @@ const foodItemObj = Joi.object().keys({
 
 const Schema = {
   placeOrderSchema: {
-    phoneNo: validatePhone.string().phoneNumber().min(11).max(13)
-      .required(),
+    phoneNo: Joi.number().integer().positive().required(),
     address: Joi.string().min(7).max(50).required(),
     foodItems: Joi.array().items(foodItemObj).required(),
   },
@@ -21,22 +17,23 @@ const Schema = {
     status: Joi.string().valid('Complete', 'Process', 'Cancel').required()
   },
   idSchema: {
-    id: Joi.number().positive().required()
+    id: Joi.number().integer().positive().required()
   },
   authSchema: {
-    firstName: Joi.string().alphanum().min(3).max(15)
+    firstName: Joi.string().regex(/^[a-zA-Z]+$/i, { name: 'a-z and A-Z without space' }).min(3).max(15)
       .required(),
-    lastName: Joi.string().alphanum().min(3).max(15)
+    lastName: Joi.string().regex(/^[a-zA-Z]+$/i, { name: 'a-z and A-Z without space' }).min(3).max(15)
       .required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).max(10).required()
   },
 
   mealSchema: {
-    foodName: Joi.string().min(3).max(30)
+    foodName: Joi.string().regex(/^[a-zA-Z ]+$/i, { name: 'a-z and A-Z' }).min(3).max(18)
       .required(),
-    price: Joi.number().positive().required(),
-    category: Joi.string().min(8).max(15).required(),
+    price: Joi.number().positive().max(999999).required(),
+    category: Joi.string().regex(/^[a-zA-Z ]+$/i, { name: 'a-z and A-Z' }).min(8).max(15)
+      .required(),
     imageUrl: Joi.string().uri().min(11)
       .required()
   },
