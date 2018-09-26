e3e8a8b60a5b50508d11791503518089c33209e0
ah-med <ahmedomosanya@gmail.com>
2018-09-26 20:03:34 +0100

    feat(orders) - admin get all orders
    - add test for GET /orders endpoint
    
    [Finishes #160630154]

--- server/test/controllers/OrderControllers.js
+++ server/test/controllers/OrderControllers.js
@@ -194,3 +194,66 @@ describe('OrderController.getOrder', () => {
       });
   });
 });
+
+describe('OrderController.getAllOrder', () => {
+  it('should get all orders', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'admin@mail.com', password: process.env.ADMIN_PASSWORD
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        adminToken = bearer + res.body.data.token;
+        chai.request(app)
+          .post('/api/v1/menu')
+          .set('Authorization', adminToken)
+          .send({
+            foodName: 'Chicken Burger30',
+            price: 1200,
+            imageUrl: 'https://res.cloudinary.com/ah-med/image/upload/v1537442340/fastfoodImage/continental/burger-chicken.jpg',
+            category: 'Continental'
+          })
+          .end((err, res) => {
+            expect(res.status).to.equal(201);
+            const { foodId } = res.body.data[0];
+            chai.request(app)
+              .post('/api/v1/auth/signup')
+              .send({
+                firstName: 'afirstname30', lastName: 'alastname30', email: 'newUser30@mail.com', password: '12345678'
+              })
+              .end((err, res) => {
+                expect(res.status).to.equal(201);
+                chai.request(app)
+                  .post('/api/v1/auth/login')
+                  .send({
+                    email: 'newUser30@mail.com', password: '12345678'
+                  })
+                  .end((err, res) => {
+                    expect(res.status).to.equal(200);
+                    userToken = bearer + res.body.data.token;
+                    chai.request(app)
+                      .post('/api/v1/orders')
+                      .set('Authorization', userToken)
+                      .send({
+                        phoneNo: '12345678901', address: 'my home town', foodItems: [{ foodId, quantity: 4 }],
+                      })
+                      .end((err, res) => {
+                        expect(res.status).to.equal(201);
+                        chai.request(app)
+                          .get('/api/v1/orders/')
+                          .set('Authorization', adminToken)
+                          .end((err, res) => {
+                            expect(res.status).to.equal(200);
+                            expect(res.body).to.have.property('data');
+                            expect(res.body.data[0]).to.have.property('status');
+                            expect(res.body.data[0].status).to.equal('New');
+                            done();
+                          });
+                      });
+                  });
+              });
+          });
+      });
+  });
+});
